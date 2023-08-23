a8781deaa56fdf07b6847d847d1280b78e876fe7
joaovictorst <joaunvictor23@hotmail.com>
2023-08-23 19:34:07 -0300

    adds

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('<h1>minha lista de tarefas</h1>');
+});
+
+app.get('/json', (req, res) =>{
+    res.json({title: 'tarefa X', done: true});
 });
 
 app.listen(3000, () => {
     console.log('servidor foi iniciado!');
 });
-
-
-app
