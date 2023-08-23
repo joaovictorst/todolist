aa268ba088e4b663a97819f4e451782fda29053f
joaovictorst <joaunvictor23@hotmail.com>
2023-08-23 17:35:28 -0300

    melhorias

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>minha lista de tarefas</h1>');
+});
+
+app.listen(3000, () => {
+    console.log('servidor foi iniciado!');
+});
